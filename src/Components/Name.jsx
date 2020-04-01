d16beb721922add4e0d991baa11b8011f66e41fa
a-kv <aakvasovaa@gmail.com>
2020-04-01 15:44:16 +0300

    Without description

---
+++ src/Components/Name.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Name.css';
+
+const Name = (props) => {
+    let div;
+    return (
+        <div className="Name">
+            Kvasova Anna Alexandrovna
+            </div>
+         );
+}
+
+export default Name;
